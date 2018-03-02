50c33b3c044aa01dddd910d8fa808fdf93a48ae1
arash <Arash Kiani>
2018-03-02 14:55:05 +0000

    update gitignore to hide secrets and compile and deploy contract on rinkeby network

---
+++ lessons/Inbox/deploy.js
@@ -0,0 +1,30 @@
+const HDWalletProvider = require('truffle-hdwallet-provider')
+const Web3 = require('web3');
+const {interface, bytecode} = require('./compile')
+const secrets = require("./secrets.json");
+/* Main Ethereum Network
+https://mainnet.infura.io
+Test Ethereum Network (Ropsten)
+https://ropsten.infura.io
+Test Ethereum Network (Rinkeby)
+https://rinkeby.infura.io
+Test Ethereum Network (Kovan)
+https://kovan.infura.io
+IPFS Gateway
+https://ipfs.infura.io
+IPFS RPC
+https://ipfs.infura.io:5001 */
+const provider = new HDWalletProvider(
+  secrets.mnemonic,
+  'https://rinkeby.infura.io/' + secrets.infura
+);
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+  console.log(accounts);
+  const result = await new web3.eth.Contract(JSON.parse(interface)).deploy({data: bytecode, arguments: ['HI there!']}).send({gas: '1000000', from: accounts[0]});
+  console.log(result.options.address);
+}
+
+deploy();
